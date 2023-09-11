b03a026a01f3a5b2316c3b1f90ac504fd573e6c9
asayke <noilzep@gmail.com>
2023-09-11 15:10:30 +0300

    Initial commit

---
+++ src/main/java/com/asayke/CharFrequencyApplication.java
@@ -0,0 +1,11 @@
+package com.asayke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharFrequencyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CharFrequencyApplication.class, args);
+    }
+}
---
+++ src/test/java/com/asayke/CharFrequencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asayke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharFrequencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
